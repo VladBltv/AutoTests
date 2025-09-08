0370e73e429ee23311aba6d073638a836bb9bb19
Vlad <exz1st1337@gmail.com>
2025-09-08 23:41:26 +0300

    new

--- tests/test_description.py
+++ tests/test_description.py
@@ -1,5 +1,8 @@
 from playwright.sync_api import sync_playwright, expect
+import pytest
 
+ver = 1.0
+@pytest.mark.skipif(ver == 1.0, reason='Для версии 1.0 тест не доступен')
 def test_description():
     '''
     Проверяем что все аккардеоны закрыты
@@ -14,6 +17,16 @@ def test_description():
         for i in range(count):
             accordionitem = page.locator(f'//div[@data-bui-id="AccordionItem"][{i+1}]').locator('xpath=.//div[@style]')
             expect(accordionitem).to_have_attribute('style', 'max-height:0')
+        context.storage_state(path='new_context.json')
 
+@pytest.mark.skip(reason='По кайфу')
 def test_new():
+    assert 1 == 1
+
+@pytest.mark.xfail(reason='Ну вот так вот')
+def test_new1():
+    assert 1 == 2
+
+@pytest.mark.xfail
+def test_new2():
     assert 1 == 1
